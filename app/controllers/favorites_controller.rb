025e9e9d6d75bc42ef11d5312ba66d0905ebf23f
Daiki <daqidiye@gmail.com>
2024-10-09 14:13:00 +0900

    second_commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,27 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_blog
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @favorite = current_user.favorites.create(blog: @blog)
+    @blog.reload
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+    @favorite.destroy if @favorite
+    @blog.reload
+    respond_to do |format|
+      format.js
+    end
   end
 
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
 end
